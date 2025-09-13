28263c26d36eba5d04dea48ba5f15010e016f7a3
100-days-of-code <singhishita2007@gmail.com>
2025-09-13 20:35:33 +0530

    code

---
+++ Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+
+    printf("Enter second number: ");
+    scanf("%d", &b)
+
+
+    sum = a + b;
+
+    
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
+c:\Users\ISHITA SINGH\OneDrive\Pictures\Screenshots\Screenshot 2025-09-13 203427.png
