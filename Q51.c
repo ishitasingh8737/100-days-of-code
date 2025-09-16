e940bf3e200238bc5e8266389181b0617c99d086
100-days-of-code <singhishita2007@gmail.com>
2025-09-16 19:14:53 +0530

    code

--- Q51.c
+++ Q51.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 int main() {
-    int i, j;
+    int i, j, n = 5;
+
     
     for (i = 5; i >= 1; i--) {
         
+        for (j = 1; j < i; j++) {
+            printf(" ");
+        }
         for (j = i; j <= 5; j++) {
             printf("%d", j);
         }
-        printf("\n"); 
+        printf("\n");
     }
     return 0;
 }
