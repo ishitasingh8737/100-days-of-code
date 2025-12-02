aca4708aeabeedf6ac6b9cc9c398fd36ec23865b
ishitasingh8737 <singhishita2007@gmail.com>
2025-12-02 10:13:59 +0530

    code

---
+++ Day_100/Q_150.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr;
+    ptr = &s;   
+    printf("Enter student details (Name Roll Marks): ");
+    scanf("%s %d %d", ptr->name, &ptr->roll, &ptr->marks);
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d\n",
+           ptr->name, ptr->roll, ptr->marks);
+    return 0;
+}
